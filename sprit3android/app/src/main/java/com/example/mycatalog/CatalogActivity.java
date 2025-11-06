1f3284f301c7d685ac33947df883c7eddeb1557a
abi4ka <nikita161787@gmail.com>
2025-11-06 12:26:00 +0100

    2 task

--- sprit3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
+++ sprit3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
@@ -1,8 +1,10 @@
 package com.example.mycatalog;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CatalogActivity extends AppCompatActivity {
@@ -10,6 +12,15 @@ public class CatalogActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_catalog);
+
+        Button btnNavegar = findViewById(R.id.btn_navegar_detalle);
+        btnNavegar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CatalogActivity.this, DetailActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
