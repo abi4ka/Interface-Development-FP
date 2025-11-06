c144715abbe9053ddc2eea53fa0513942dd32492
abi4ka <nikita161787@gmail.com>
2025-11-06 11:54:33 +0100

    initial commit

---
+++ sprit3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
@@ -0,0 +1,15 @@
+package com.example.mycatalog;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CatalogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
