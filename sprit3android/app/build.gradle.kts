073598d5e5b27efaf2c5b985f65977df82744a4d
abi4ka <nikita161787@gmail.com>
2025-11-10 10:01:32 +0100

    image circle fix

--- sprit3android/app/build.gradle.kts
+++ sprit3android/app/build.gradle.kts
@@ -34,6 +34,7 @@ android {
 dependencies {
     implementation("androidx.navigation:navigation-fragment:2.8.0")
     implementation("androidx.navigation:navigation-ui:2.8.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
